3861c481c6459c83c01737cc1cc35f9f2a1f4d99
J K <kj688895@gmail.com>
2020-09-17 20:11:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,8 @@
-reverse_each_word("Hello there, and how are you?")
-reverse_each_word.reverse
+def reverse_each_word(string)
+  array = string.split(" ")
+  array_one = []
+  array.collect do |string|
+    array_one << string.reverse
+  end
+  array_one.join(" ")
+end
